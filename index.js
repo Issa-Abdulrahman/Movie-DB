3be8109dd3488fe4042502fa7a40800c7ae3e910
Issa <isaiahabdulrahman@gmail.com>
2023-09-24 18:41:08 +0300

    step 3

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express();
+const port= 10452
 app.get('/',(req,res) => (
     res.json("OK")
-))
-app.listen(10452);
+));
+
+app.get('/test',(req,res) => (
+    res.json({status:200,message:"ok"})
+));
+
+const now= new Date();
+app.get('/time',(req,res)=>{
+    res.json({status:200,message:now.getHours() +":"+now.getSeconds()})
+});
+
+app.listen(port, () =>{
+    
+})
+
+
